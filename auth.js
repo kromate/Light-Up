c4a7b1e17059ff7b683b879bba0bf567ae5235b9
kromate <akpananthony33@gmail.com>
2020-04-07 00:53:33 +0100

    stuff

--- auth.js
+++ auth.js
@@ -1,3 +1,39 @@
+//listen for Auth status changes
+auth.onAuthStateChanged(user=>{
+    console.log(user);
+    if(user == null){
+        window.location = 'login.html'
+    }
+    else{
+        window.location = 'dashboard.html'}
+})
+
+
+    const config = {
+        apiKey: "AIzaSyB7MbhzQ2yohv7MOjLy0rvx2n7DHvv8f5c",
+        authDomain: "light-up-eb627.firebaseapp.com",
+        databaseURL: "https://light-up-eb627.firebaseio.com",
+        projectId: "light-up-eb627",
+        storageBucket: "light-up-eb627.appspot.com",
+        messagingSenderId: "478749966375",
+        appId: "1:478749966375:web:947b648e159b438dfd0e02",
+        measurementId: "G-T3C0GSGT7B"
+    };
+    firebase.initializeApp(config);
+    // making refs 
+    const auth = firebase.auth();
+    const db = firebase.firestore();
+
+    //update firestor settings
+
+    db.settings({timestampsInSnapshots: true })
+
+
+
+
+
+
+
 const signupForm = document.querySelector('#registrationform');
 signupForm.addEventListener('submit',(e)=>{
     e.preventDefault();
@@ -31,8 +67,24 @@ loginForm.addEventListener('submit', (e)=>{
     firebase.auth().signInWithEmailAndPassword(email, password).then(cred=>{
         console.log(cred.user);
         if(cred.user != null){
-            window.location = 'dashboard.html';
+            window.location = 'dashboard.html'; 
         }
         else alert('something went wrong')
     })
+});
+
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e)=>{
+    e.preventDefault();
+    auth.signOut().then(()=>{
+        console.log('User signed out');
+        window.location = 'login.html';
+    })
 })
+
+const userEmail = document.querySelector('#accName')
+
+const setupUI = (user)=>{
+    const html = `${user.email}`;
+    userEmail.innerHTML = html;
+}
